1dff291d3b69b0ebfc9182d8b09ad069afccd058
Raphael Leardini <raphaelleardini@hotmail.com>
2019-08-22 09:57:18 -0300

    Post, AuthorDTO projection and PostRepository implemented

--- src/main/java/com/workshop/workshopmongo/config/Instantiation.java
+++ src/main/java/com/workshop/workshopmongo/config/Instantiation.java
@@ -1,12 +1,17 @@
 package com.workshop.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.workshop.workshopmongo.domain.Post;
 import com.workshop.workshopmongo.domain.User;
+import com.workshop.workshopmongo.dto.AuthorDTO;
+import com.workshop.workshopmongo.repository.PostRepository;
 import com.workshop.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,18 +20,29 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		// Salvar primeiro para que tenham id proprio
+		userRepository.saveAll(Arrays.asList(maria, alex, bob)); 
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para Sao Paulo, Abracos!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
