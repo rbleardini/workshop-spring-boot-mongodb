c9940f3b5d4e79a1f5f27359970f2f7b59195086
Raphael Leardini <raphaelleardini@hotmail.com>
2019-08-22 11:05:00 -0300

    Adding comments to posts

---
+++ src/main/java/com/workshop/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.workshop.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.workshop.workshopmongo.domain.Post;
+import com.workshop.workshopmongo.repository.PostRepository;
+import com.workshop.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired //mecanismo de injecao de dependencia automatica do spring
+	private PostRepository repo; 
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+}
