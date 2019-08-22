41b5378fe980cb7b211a564143ae2825492d36e7
Raphael Leardini <raphaelleardini@hotmail.com>
2019-08-21 22:20:14 -0300

    Updating user with PUT

--- src/main/java/com/workshop/workshopmongo/services/UserService.java
+++ src/main/java/com/workshop/workshopmongo/services/UserService.java
@@ -35,6 +35,17 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		Optional<User> newObj = repo.findById(obj.getId());
+		updateData(newObj.get(), obj);
+		return repo.save(newObj.get());
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
