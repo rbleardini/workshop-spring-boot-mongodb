1763f96f9bbb4041da8e591a5c14f36d3f69179d
Raphael Leardini <raphaelleardini@hotmail.com>
2019-08-22 21:56:57 -0300

    Using query methods

--- src/main/java/com/workshop/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/workshop/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.workshop.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import com.workshop.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
+	// Isto é um query method, é um metodo definido
+	// automaticamente no spring-data a partir do 'sufixo' no nome do método
+	// Ex: 'Containing' 'After' 'Before' 'In' 'IgnoreCase'
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
