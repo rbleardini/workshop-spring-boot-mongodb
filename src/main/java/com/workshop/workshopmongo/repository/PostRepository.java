08b8ae8e67cb5d0d9b04a5d7b37cc62471b9b0b7
Raphael Leardini <raphaelleardini@hotmail.com>
2019-08-24 09:25:30 -0300

    Multicriteria search implemented

--- src/main/java/com/workshop/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/workshop/workshopmongo/repository/PostRepository.java
@@ -1,8 +1,10 @@
 package com.workshop.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.workshop.workshopmongo.domain.Post;
@@ -10,8 +12,14 @@ import com.workshop.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	// Isto é um query method, é um metodo definido
 	// automaticamente no spring-data a partir do 'sufixo' no nome do método
 	// Ex: 'Containing' 'After' 'Before' 'In' 'IgnoreCase'
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: {$lte: ?2} }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
